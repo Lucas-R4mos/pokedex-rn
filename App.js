63cfd05fd3eafdc1b03ff1bf6792e0f665d4c146
Lucas Ramos <lucas.ramos@lsbd.ufc.br>
2021-08-30 15:37:26 -0300

    First commit with the basics.

--- App.js
+++ App.js
@@ -1,21 +1,182 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	SafeAreaView,
+	KeyboardAvoidingView,
+	ActivityIndicator,
+	Modal,
+	Button,
+	Image
+} from 'react-native';
+import { Dropdown } from 'sharingan-rn-modal-dropdown';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [isLoading, setLoading] = useState(true)
+	const [pokeList, setPokeList] = useState([])
+	const [selection, setSelection] = useState()
+	const [modalVisible, setModalVisible] = useState(false)
+	const [pokeData, setPokeData] = useState({})
+	const [isModalLoading, setModalLoading] = useState(true)
+
+	useEffect(() => {
+		loadData()
+	}, [])
+
+	function capitalizeFirstLetter(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+
+	const loadData = async () => {
+		const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+		const data = await res.json()
+		const filteredData = data.results.map((poke) => {
+			let pokeInitData = {}
+			pokeInitData.label = capitalizeFirstLetter(poke.name)
+			pokeInitData.value = poke.url
+
+			return pokeInitData
+		})
+
+		await setPokeList(filteredData)
+		await setLoading(false)
+	}
+
+	const handleDropdownChange = async (value) => {
+		await setModalLoading(true)
+		await setModalVisible(true)
+
+		try {
+			const res = await fetch(value)
+			const data = await res.json()
+
+			await setPokeData(data)
+		} catch (err) {
+			if (err) console.log('Error on request:', err)
+		}
+
+		await setSelection(value)
+		await setModalLoading(false)
+
+	}
+
+	if (isLoading) {
+		return (
+			<SafeAreaView style={styles.mainContainer}>
+				<ActivityIndicator size={100} color='#212121' />
+			</SafeAreaView>
+		)
+	}
+
+	return (
+		<SafeAreaView style={styles.mainContainer}>
+			<KeyboardAvoidingView style={styles.secContainer}>
+				<View style={styles.mainCard}>
+					<Text style={styles.Header} >Pokedex</Text>
+					<Text style={{ paddingBottom: 20 }}>
+						Choose a Pokémon:
+					</Text>
+					<Dropdown
+						mainContainerStyle={styles.dropdown}
+						label='I choose you!'
+						data={pokeList}
+						// enableSearch
+						value={selection}
+						onChange={handleDropdownChange}
+						disableSort
+					/>
+				</View>
+			</KeyboardAvoidingView>
+			<Modal
+				animationType="slide"
+				transparent={true}
+				visible={modalVisible}
+				onRequestClose={() => { setModalVisible(!modalVisible) }}
+			>
+				<View
+					style={styles.modalView}
+				>
+					{
+						isModalLoading
+							? <ActivityIndicator size={75} color='#212121' />
+							: <>
+								<View style={styles.dataContainer}>
+									<Image
+										source={{
+											uri: pokeData.sprites.other["official-artwork"].front_default,
+											width: 150,
+											height: 150,
+											method: 'GET',
+										}}
+									/>
+									<Text>{capitalizeFirstLetter(pokeData.name)}</Text>
+									<Text>Type(s):</Text>
+									{
+										pokeData.types.map((type) => {
+											return <Text>- {capitalizeFirstLetter(type.type.name)}</Text>
+										})
+									}
+								</View>
+								<Button
+									onPress={() => { setModalVisible(false) }}
+									title={'x'}
+									color='#121212'
+									
+								/>
+							</>
+					}
+				</View>
+			</Modal>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	mainContainer: {
+		flex: 1,
+		flexDirection: 'column',
+		backgroundColor: '#FB1B1B',
+		alignItems: 'center',
+		justifyContent: 'center',
+		minWidth: '100%',
+		paddingTop: 100
+	},
+	secContainer: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	Header: {
+		fontSize: 50
+	},
+	mainCard: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	dropdown: {
+		width: 200
+	},
+	modalView: {
+		backgroundColor: "white",
+		padding: 20,
+		alignItems: "center",
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+		elevation: 5,
+		position: 'absolute',
+		bottom: 0,
+		width: '100%',
+		borderTopLeftRadius: 30,
+		borderTopRightRadius: 30,
+	},
+	dataContainer: {
+
+	}
 });
